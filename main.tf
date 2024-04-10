a2dfa6580a941edbbdee1c873f1a249a5eb7c635
paulonos <paulonos4@hotmail.com>
2024-04-09 22:51:35 -0400

    block port 80.

--- main.tf
+++ main.tf
@@ -68,15 +68,15 @@ resource "aws_security_group" "SG" {
 
 
 
-  ingress {
-    description = "Allow httpd"
+  # ingress {
+  #   description = "Allow httpd"
 
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  #   from_port        = 80
+  #   to_port          = 80
+  #   protocol         = "tcp"
+  #   cidr_blocks      = ["0.0.0.0/0"]
   
-  }
+  # }
 
 
 
